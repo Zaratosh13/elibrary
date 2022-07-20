9d6dd10bd41eb617facb7bd67e8d4a72ecab546a
Zaratosh13 <105578171+Zaratosh13@users.noreply.github.com>
2022-07-20 12:17:26 +0530

    Add project files.

---
+++ WebApplication1/Sign Up.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+
+    public partial class Sign_Up : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // sign up button is click
+        protected void Button2_Click (object sender, EventArgs e)
+        {
+           signUpNewUser();
+
+        }
+
+        void signUpNewUser()
+        {
+            // Response.Write("<script>alert('Testing');</script>");
+
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("INSERT INTO member_master_table (full_name,dob,contact_no,email,state,city,pincode,full_address,member_id,password,account_status) valus(@full_name,@dob,@contact_no,@email,@state,@city,@pincode,@full_address,@member_id,@password,@account_status) ", con);
+
+                cmd.Parameters.AddWithValue("@full_name", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@dob", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@contact_no", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@email", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@state", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@city", TextBox6.Text.Trim());
+                cmd.Parameters.AddWithValue("@pincode", TextBox7.Text.Trim());
+                cmd.Parameters.AddWithValue("@full_address", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@member_id", TextBox8.Text.Trim());
+                cmd.Parameters.AddWithValue("@password", TextBox9.Text.Trim());
+                cmd.Parameters.AddWithValue("@account_status", "pending");
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Sign Up Successful. Go to User Login to Login');</Script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+    }
+}
---
+++ WebApplication1/Site1.Master.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void LinkButton5_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        protected void LinkButton6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("admin login.aspx");
+        }
+
+        protected void LinkButton7_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminauthormanagement.aspx");
+        }
+
+        protected void LinkButton8_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Publisher Management.aspx");   
+        }
+
+        protected void LinkButton9_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminbookinventory.aspx");
+        }
+
+        protected void LinkButton10_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminbookissuingpage.aspx");
+        }
+
+        protected void LinkButton11_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Member Management.aspx");
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("user login.aspx");
+        }
+
+        protected void linkButton4_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("View Books.aspx");
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Sign Up.aspx");
+        }
+
+
+    }
+}
